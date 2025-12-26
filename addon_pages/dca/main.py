37fe318d240b3179b3dbe19da1c87277f3d2c91a
mrAyumu <sethayumu@gmail.com>
2025-12-26 06:56:11 -0700

    Update main.py

--- addon_pages/dca/main.py
+++ addon_pages/dca/main.py
@@ -561,4 +561,5 @@ def generate_summary_statistics(decline_constants: pd.DataFrame) -> dict:
 
 if __name__ == "__main__":
     import uvicorn
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=8000)
